6c07ccc620ea1a021422eb884ce9cb77fe1da40f
Higor Costa <higorcosta972@gmail.com>
2023-09-05 20:02:33 -0300

    Adiciona array de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
 import React from 'react';
 import './App.css';
 
+const tarefas = [
+  {
+    id: 1,
+    nome: 'Curso completo de JavaScript on-line' ,
+    concluida: true
+  },
+  {
+    id: 2,
+    nome: 'Corra pelo parque 3x',
+    concluida: false 
+  },
+  {
+    id: 3,
+    nome: '10 minutos de meditação',
+    concluida: false 
+  },
+  {
+    id: 4,
+    nome: 'Ler por 1h',
+    concluida: false 
+  },
+  {
+    id: 5,
+    nome: 'Pegue mantimentos',
+    concluida: false 
+  },
+  {
+    id: 6,
+    nome: 'Conclua Todo App no Frontend Mentor',
+    concluida: false 
+  }
+]
+
 function App() {
   return <></>;
 }
