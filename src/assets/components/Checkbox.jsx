9151f25c773dc00c14b34862f2de92876c4f5013
Higor Costa <higorcosta972@gmail.com>
2023-09-06 12:27:04 -0300

    Criar componente "Checkbox"

---
+++ src/assets/components/Checkbox.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Estilo
+import styles from './Checkbox.module.css';
+
+const Checkbox = () => {
+  return <input type="checkbox" className={styles} />;
+};
+
+export default Checkbox;
