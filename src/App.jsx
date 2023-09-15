e6123b75476edfd1ea96dde3873b5eb16349a7a6
Higor Costa <higorcosta972@gmail.com>
2023-09-15 20:23:39 -0300

    Exclui array inicial de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -11,39 +11,6 @@ import useMedia from './assets/hooks/useMedia';
 // Estilo
 import styles from './App.module.css';
 
-const tarefas = [
-  {
-    id: 1,
-    nome: 'Curso completo de JavaScript on-line',
-    concluida: true,
-  },
-  {
-    id: 2,
-    nome: 'Corra pelo parque 3x',
-    concluida: false,
-  },
-  {
-    id: 3,
-    nome: '10 minutos de meditação',
-    concluida: false,
-  },
-  {
-    id: 4,
-    nome: 'Ler por 1h',
-    concluida: false,
-  },
-  {
-    id: 5,
-    nome: 'Pegue mantimentos',
-    concluida: false,
-  },
-  {
-    id: 6,
-    nome: 'Conclua Todo App no Frontend Mentor',
-    concluida: false,
-  },
-];
-
 const App = () => {
   const dispositivoMovel = useMedia('(max-width: 630px)');
   const [listaTarefas, setListaTarefas] = React.useState(() => {
